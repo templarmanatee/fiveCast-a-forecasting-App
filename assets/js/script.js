b535701cd7691cc386e7a72aca05233f7efd5450
DylanF <dylanbrad10@gmail.com>
2023-02-10 02:26:13 -0500

    wip button generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-const cityBtn = document.querySelector('#city-btn');
-const searchFormEl = document.querySelector('#search-form');
+const cityBtn = document.querySelector("#city-btn");
+const searchFormEl = document.querySelector("#search-form");
 
 const searchApi = async (city) => {
   const weatherQueryUrl = "https://api.openweathermap.org/data/2.5/weather";
@@ -17,14 +17,49 @@ const searchApi = async (city) => {
   return weatherData;
 };
 
+const saveHistory = (city) => {
+  const savedList = localStorage.getItem("history");
+  savedList.cities.push(city);
+  localStorage.setItem("history", JSON.stringify(savedList));
+  loadButtons();
+};
+
+const loadButtons = () => {
+  const buttonBlock = document.querySelector("#history");
+  const history = localStorage.getItem('history'); 
+  if (!history) {
+    localStorage.setItem("history", JSON.stringify({
+        cities: []
+    }));
+  }
+
+  for (city in history) {
+    const button = document.createElement("button");
+    button.classList.add("btn");
+    button.classList.add("btn-primary");
+    button.innerHTML = city;
+
+    buttonBlock.appendChild(button);
+  }
+};
+
 async function handleInput(event) {
   event.preventDefault();
 
-  const citySearchBar = document.querySelector('#city-search-bar');
+  const citySearchBar = document.querySelector("#city-search-bar");
 
   const inputCity = citySearchBar.value;
   const weatherData = await searchApi(inputCity);
-  console.log(weatherData)
-};
+  console.log(weatherData);
 
-searchFormEl.addEventListener('submit', handleInput);
+  const history = JSON.parse(localStorage.getItem('history')); 
+  const updatedHistory = history.cities;
+  updatedHistory.push(inputCity);
+
+  localStorage.setItem("history", JSON.stringify(updatedHistory));
+  loadButtons();
+}
+
+searchFormEl.addEventListener("submit", handleInput);
+loadButtons();
+
